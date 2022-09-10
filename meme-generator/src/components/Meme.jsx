4f37e40d11b528f2a40ee4a5fd77ed41955bf6e7
sahilyeole <sahilyeole93@gmail.com>
2022-09-10 13:58:32 +0530

    added text on meme

--- meme-generator/src/components/Meme.jsx
+++ meme-generator/src/components/Meme.jsx
@@ -1,30 +1,72 @@
-import React, {useState} from "react";
-import "../styles/Meme.css"
+import React, { useState } from "react";
+import "../styles/Meme.css";
 import "../styles/Button.css";
-import memesData from "../memesData"
+import memesData from "../memesData";
 export default function Meme(props) {
+  const memesArray = memesData.data.memes;
+  const randomNum = Math.floor(Math.random() * memesArray.length);
+  let url = memesArray[randomNum].url;
 
-  const [imageUrl, setImageUrl] = useState("")  
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: url,
+  });
+  const [imageUrl, setImageUrl] = useState("");
 
-  function clickHandler(){
-    const memesArray = memesData.data.memes
-    const randomNum = Math.floor(Math.random() * memesArray.length)
-    let url = memesArray[randomNum].url
-    setImageUrl(url)
+  function changeHandler(event) {
+    const { name, value } = event.target;
+    setMeme((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
   }
 
-  return <div className="meme">
-    <form className="form">
-      <div className="inp-container">
-      <input className="inputs" type="text" id="inp1" placeholder="Top Text"/>
-      <input className="inputs" type="text" id="inp2" placeholder="Bottom Text"/>
-      </div>
-      <div className="button-body">
-      <button className="button" type="button" onClick={clickHandler}>Get a new meme image </button>
+  function clickHandler() {
+    setImageUrl(url);
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+    console.log(meme);
+  }
+
+  return (
+    <div className="meme">
+      <form className="form">
+        <div className="inp-container">
+          <input
+            className="inputs"
+            type="text"
+            id="inp1"
+            placeholder="Top Text"
+            name="topText"
+            value={meme.topText}
+            onChange={changeHandler}
+          />
+          <input
+            className="inputs"
+            type="text"
+            id="inp2"
+            placeholder="Bottom Text"
+            name="bottomText"
+            value={meme.bottomText}
+            onChange={changeHandler}
+          />
+        </div>
+        <div className="button-body">
+          <button className="button" type="button" onClick={clickHandler}>
+            Get a new meme image
+          </button>
+        </div>
+        <div className="img-container">
+          <img src={meme.randomImage} className="meme-image" />
+          <h2 className="meme--text top">{meme.topText}</h2>
+          <h2 className="meme--text bottom">{meme.bottomText}</h2>
+        </div>
+      </form>
     </div>
-    <div className="img-container">
-    <img src={imageUrl} className="meme-image"/>
-    </div>
-    </form>
-  </div>;
+  );
 }

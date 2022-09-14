c1eb04102bbd61072002dd59ad52e0cbb6ef92d6
sahilyeole <sahilyeole93@gmail.com>
2022-09-14 22:20:32 +0530

    added stats

--- bank-modern-app/src/components/GetStarted.jsx
+++ bank-modern-app/src/components/GetStarted.jsx
@@ -8,7 +8,7 @@ const GetStarted = () => {
       <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full `}>
 
       <div className={`${styles.flexStart} flex-row`}>
-        <p className='font-poppins text-gradient font-medium text-[18px] leading-[23px] '><span>Get</span></p>
+        <p className='font-poppins text-gradient font-medium text-[18px] leading-[23px] mr-2 '><span>Get</span></p>
         <img src={arrowUp} className="w-[23px] h-[23px] object-contain" />
       </div>
       <p className='font-poppins text-gradient font-medium text-[18px] leading-[23px] '><span>Started</span></p>
